ad8b11dfa5f7851f10ee2820e3e4b40321a692e0
Miguel <miguel.mtz.quevedo@gmail.com>
2018-03-25 17:18:07 -0600

    first geometry and world definitions

--- source/app.d
+++ source/app.d
@@ -1,6 +1,42 @@
 import std.stdio;
+import abst.world;
+import geometry;
+
+class Rock : IElement
+{
+    override string toString()
+    {
+	return "i am rock";
+    }
+};
+
+class World100 : IWorld
+{
+    public this()
+    {
+    }
+
+    Vector!uint getSize() 
+    {
+	Vector!uint size = { x: 100, y: 100, z: 100 };
+	return size;
+    }
+
+    IElement getElement(int i, int j, int k)
+    {
+	return rock;
+    }
+
+    Rock rock;
+};
+
+void receiveWorld(IWorld world)
+{
+    writeln("received a world of size ", world.getSize());
+}
 
 void main()
 {
-    writeln("aguante D");
+    IWorld world = new World100();
+    receiveWorld(world);
 }
