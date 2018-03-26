165319eff32978e789096f0fb9c10cf0a2a1208f
Miguel <miguel.mtz.quevedo@gmail.com>
2018-03-25 19:33:12 -0600

    Commented world.d

--- source/abst/world.d
+++ source/abst/world.d
@@ -2,12 +2,20 @@ module abst.world;
 
 import geometry;
 
+/*
+Common interface for world elements.
+*/
 interface IElement
 {
 };
 
+/*
+Common interface for 'World's, which are 3D array's of 'IElements'
+*/
 interface IWorld
 {
+    // Returns the element in the position (i, j, k)
     IElement getElement(int i, int j, int k);
+    // Returns the size of the world
     Vector!uint getSize();
 };
