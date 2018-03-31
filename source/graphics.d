85b25bbcd5ab429ddfca5766c6fe0765add6da02
Miguel <miguel.mtz.quevedo@gmail.com>
2018-03-30 18:47:13 -0600

    add comments graphics.d

--- source/graphics.d
+++ source/graphics.d
@@ -10,6 +10,18 @@ import derelict.sdl2.sdl;
 
 import viare.geometry;
 
+
+/*
+@square function.
+    Squares the given number, returns the result.
+*/
+double //square of @x
+square
+    (double x/*number to be squared*/)
+{
+    return x * x;
+}
+
 static this()
 {
     DerelictSDL2.load();
@@ -25,10 +37,22 @@ static ~this()
 {
 }
 
+/*
+@Window class.
+    Represents a named rectangular drawing area.
+*/
 class Window
 {
     public:
-	this(string name, uint width, uint height)
+
+    /*
+    @Window constructor.
+	Constructs a new window with the specified dimensions (@width x @height) and name @name.
+    */
+	this
+	    (string name, // name of the window to be constructed
+	    uint width, // width of the window to be constructed
+	    uint height) // height of the window to be constructed
 	{
 	    m_window = SDL_CreateWindow(name.toStringz(),
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -39,30 +63,73 @@ class Window
 
 	    DerelictGL3.reload();
 	}
+    // Deallocates resources.
 	~this()
 	{
 	    SDL_DestroyWindow(m_window);
 	}
 
     private:
+    // Internal SDL2 window handle.
 	SDL_Window* m_window;
+    // Internal SDL2-OpenGL Context handle.
 	SDL_GLContext m_glContext;
 }
 
 
+/*
+@Shader class.
+    Represents a 'shader' OpenGL object.
+	A shader is *part* of a program ought to be executed by the GPU.
+	This class serves as a way to compile and use those program parts.
+
+	The "whole" @Program is another OpenGL object constructed by assembling
+	many 'Shader's.
+*/
 class Shader
 {
     public:
+    /*
+    @Shader.Type enum.
+    	Contains the types of @Shader there can be.
+    */
 	enum Type
 	{
-	    Vertex, Fragment
+	    // Vertex Processing
+	    Vertex,
+	    // Fragment Processing
+	    Fragment
 	};
 
+    /*
+    @Shader.type method.
+	Returns the type of the shader @this
+
+    Inputs:
+	none
+    Ouputs:
+	return(@Shader.Type):
+	    shader type of shader represented by @this
+    */
 	@property Type type()
 	{
 	    return m_type;
 	}
 
+    /*
+    @Shader constructor.
+	Constructs a new shader of type @type, using as source code the file pointed to by
+	@sourcePath
+
+    Inputs:
+	@type(@Shader.Type):
+	    Type of the shader to be constructed
+	@sourcePath(@string)
+	    String representing a path to a file containing the source code which will be used as
+	    source for the constructed shader
+    Outputs:
+	Constructs @this
+    */
 	this(Shader.Type type, string sourcePath)
 	{
 	    m_type = type;
@@ -70,9 +137,25 @@ class Shader
 	}
 
     private:
+    // Internal OpenGL Handle (aka name) to the shader object.
 	GLuint m_shaderGlName;
+    // Type of the shader.
 	Type m_type;
 
+    /*
+    @Shader.compileShader function.
+	Compiles a shader ot type @type, using as source code the file pointed to by @sourcePath and
+	returns the name of the opengl object representing the compiled shader.
+
+    Inputs:
+	@type(@Shader.Type):
+	    type of shader to be compiled
+	@sourcePath(@string):
+	    path to the shader's source code
+    Outputs:
+	return(@GLuint):
+	    Opengl name of the compiled shader
+    */
 	static GLuint compileShader(Type type, string sourcePath)
 	{
 	    GLuint shaderName = glCreateShader(typeToGlenum(type));
@@ -102,6 +185,17 @@ class Shader
 	    return shaderName;
 	}
 
+    /*
+    @Shader.typeToGlenum function.
+	Maps @Shader.Type to equivalent OpenGL native @GLenum.
+
+    Inputs:
+	@type(@Shader.Type):
+	    type value to be mapped to @GLenum
+    Outputs:
+	return(@GLenum):
+	    @GLenum equivalent of @type
+    */
 	static pure GLenum typeToGlenum(Type type)
 	{
 	    switch(type)
@@ -110,15 +204,30 @@ class Shader
 		    return GL_VERTEX_SHADER;
 		case Type.Fragment:
 		    return GL_FRAGMENT_SHADER;
-		default:
-		    return GL_VERTEX_SHADER;
 	    }
 	}
 }
 
+/*
+@Buffer class.
+    Represents a buffer opengl object.
+	A buffer opengl object is the mechanism through which data can be stored in the GPU, usually
+	vertex data of the models to be rendered.
+
+	This class eases/abstracts the interaction with this kind of opengl objects.
+*/
 class Buffer
 {
     public:
+    /*
+    @Buffer constructor.
+	Constructs a new @Buffer.
+
+    Inputs:
+	none
+    Outputs:
+	Constructs @this
+    */
 	this()
 	{
 	    glGenBuffers(1, &m_bufferName);
@@ -128,6 +237,18 @@ class Buffer
 	    glDeleteBuffers(1, &m_bufferName);
 	}
 
+    /*
+    @Buffer.bufferData method
+	Sends the data pointed to by @data, and of size @size (in bytes) to @this buffer.
+
+    Inputs:
+	@data(@void*):
+	    Pointer to the data to be sent
+	@size(@size_t):
+	    Size in bytes of the data to be sent
+    Outpus:
+	none
+    */
 	void bufferData(void* data, size_t size)
 	{
 	    bind();
@@ -146,17 +267,35 @@ class Buffer
 	}
 
     private:
+    // opengl name of the buffer managed by @this
 	GLuint m_bufferName;
 }
 
+/*
+@Vertex struct.
+    Gives the necessary information to represent a Vertex in a 3D model.
+*/
 struct Vertex
 {
+    // Position of the vertex
     Vector!float position;
+    // Color of the vertex
     Vector!float color;
 
+    /*
+	OpenGL requires us to give it the format in which data is stored in GPU.
+	Vertex.formats provides this information about this Vertex format in particular.
+
+	Note it is a *static* property of the Vertex structure, not of instances.
+
+	OpenGL thinks of every Vertex as having _attributes_, as we think of _members_ of a struct.
+	This Vertex struct contains two _attributes_, they are the @position and the @color.
+
+	See @AttributeFormat for more info about the data needed by OpenGL.
+    */
     static AttributeFormat[] formats = 
     [
-	//position format
+	//position attribute format
 	{
 	    index: 0, 
 	    size: 3, 
@@ -164,8 +303,13 @@ struct Vertex
 	    normalized: GL_FALSE, 
 	    stride: cast(GLsizei)Vertex.sizeof, 
 	    pointer: cast(void*)Vertex.position.offsetof
+	    /*
+	    EXPLANATION:
+		position is the attribute no. 0, contains 3 components each of type float which
+		won't (GL_FALSE) be normalized.
+	    */
 	},
-	//color format
+	//color attribute format
 	{
 	    index: 1, 
 	    size: 3, 
@@ -177,16 +321,46 @@ struct Vertex
     ];
 };
 
+/*
+@AttributeFormat
+    Data needed to represent a particular attribute for a Vertex.
+*/
 struct AttributeFormat
 {
+    // OpenGL identifies each attribute by an @index
     GLuint index;
+    // No. of components of this attribute
     GLint size;
+    // Data type of the components of this attribute
     GLenum type;
+    // Does it need to be _normalized_(Clipped to a range of 0.0 - 1.0)?
     GLboolean normalized;
+    // Space between each appearance of this attribute in an array of Vertices, equivalently, the
+    // size of each Vertex
     GLsizei stride;
+    // Offset to first appearance of this attribute in an array of Vertices, equivalently, the
+    // offset of this member in the Vertex structure
     const GLvoid* pointer;
 };
 
+/*
+@setup function.
+    Given the @Buffer and the @VertexArray bound to the OpenGL context, this function provides
+    format info about the attribute @format.index of the vertices in the VertexArray.
+
+    This associates the @Buffer to the @VertexArray.
+
+Inputs:
+    @format(@AttributeFormat):
+	attribute format to be given to the @VertexArray currently bound
+
+    _implicit_ _*REQUIRED*_ currently bound @VertexArray object:
+	VertexArray to be formatted
+    _implicit_ _*REQUIRED*_ currently bound @Buffer object:
+	Buffer from which to get the data pointer
+Outputs:
+    none
+*/
 void setup(AttributeFormat format)
 {
     glEnableVertexAttribArray(format.index);
