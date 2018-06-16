3c58aea5beacf78f279187fada7dc576f753fa37
Miguel <miguel.mtz.quevedo@gmail.com>
2018-06-16 14:26:06 -0500

    add heightmap generation test

--- source/models.d
+++ source/models.d
@@ -7,29 +7,29 @@ import viare.graphics;
 immutable(Vertex)[] SQUARE_VERTICES = [
     // 1st triangle
     {
-	position: [-0.5, -0.5, 0],
+	position: [-1, -1, 0],
 	uv: [0, 0]
     },
     {
-	position: [+0.5, -0.5, 0],
+	position: [+1, -1, 0],
 	uv: [1, 0]
     },
     {
-	position: [-0.5, +0.5, 0],
+	position: [-1, +1, 0],
 	uv: [0, 1]
     },
 
     // 2nd triangle
     {
-	position: [+0.5, +0.5, 0],
+	position: [+1, +1, 0],
 	uv: [1, 1]
     },
     {
-	position: [+0.5, -0.5, 0],
+	position: [+1, -1, 0],
 	uv: [1, 0]
     },
     {
-	position: [-0.5, +0.5, 0],
+	position: [-1, +1, 0],
 	uv: [0, 1]
     }
 ];
