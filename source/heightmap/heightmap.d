4b3ba8a2268e0d5ebb6ce2dc4b1fd32328c2c206
Miguel <quevangel@protonmail.com>
2018-06-26 14:23:09 -0500

    Fix heightmap identation

--- source/heightmap/heightmap.d
+++ source/heightmap/heightmap.d
@@ -4,52 +4,52 @@ import viare.heightmap.heightfunction;
 
 class HeightMap
 {
-    private:
-	float[][] m_height;
-	const uint m_width;
-	const uint m_height;
+	private:
+		float[][] m_height;
+		const uint m_xlength;
+		const uint m_ylength;
 
-    public:
-	this(uint width, uint height)
-	{
-	    m_width = width;
-	    m_height = height;
+	public:
+		this(uint width, uint height)
+		{
+			m_xlength = width;
+			m_ylength = height;
 
-	    m_height.length = width;
-	    for(uint i = 0u; i < width; i++)
-		m_height[i].length = height;
-	}
+			m_height.length = width;
+			for(uint i = 0u; i < width; i++)
+				m_height[i].length = height;
+		}
 
-	ref float opIndex(size_t i, size_t j)
-	{
-	    return m_height[i][j];
-	}
+		ref float opIndex(size_t i, size_t j)
+		{
+			return m_height[i][j];
+		}
 
-	void fillByHeightFunction(HeightFunction heightFunction)
-	{
-	    for(uint i = 0; i < m_width; i++)
-	    for(uint j = 0; j < m_height; j++)
-		this[i, j] = heightFunction(cast(float) i / m_width, cast(float) j / m_height);
-	}
+		void fillByHeightFunction(HeightFunction heightFunction)
+		{
+			for(uint i = 0; i < m_width; i++)
+				for(uint j = 0; j < m_height; j++)
+					this[i, j] = heightFunction(cast(float) i / m_width, cast(float) j / m_height);
+		}
 
-	void normalize()
-	{
-	    float lowest = this[0, 0], highest = this[0, 0];
+		void normalize()
+		{
+			float lowest = this[0, 0], highest = this[0, 0];
 
-	    for(uint i = 0; i < m_width; i++)
-	    for(uint j = 0; j < m_height; j++)
-	    {
-		if(this[i, j] < lowest)
-		    lowest = this[i, j];
-		if(this[i, j] > highest)
-		    highest = this[i, j];
-	    }
+			for(uint i = 0; i < m_width; i++)
+				for(uint j = 0; j < m_height; j++)
+				{
+					if(this[i, j] < lowest)
+						lowest = this[i, j];
+					if(this[i, j] > highest)
+						highest = this[i, j];
+				}
 
-	    float maxRelativeHeight = highest - lowest;
+			float maxRelativeHeight = highest - lowest;
 
-	    for(uint i = 0; i < m_width; i++)
-	    for(uint j = 0; j < m_height; j++)
-		this[i, j] = (this[i, j] - lowest) / maxRelativeHeight;
-	}
+			for(uint i = 0; i < m_width; i++)
+				for(uint j = 0; j < m_height; j++)
+					this[i, j] = (this[i, j] - lowest) / maxRelativeHeight;
+		}
 }
 
